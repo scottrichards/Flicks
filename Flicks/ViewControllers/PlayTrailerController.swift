ea61affc4df8141e1c8f0c3d6b7eabb2a7c73b7c
Scott Richards <scott.srichards@gmail.com>
2016-10-13 08:43:01 -0700

    Working on adding movie playback

--- Flicks/ViewControllers/PlayTrailerController.swift
+++ Flicks/ViewControllers/PlayTrailerController.swift
@@ -7,9 +7,18 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlayTrailerController: UIViewController {
+    
+    @IBOutlet weak var rewindButton: UIButton!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var fastForwardButton: UIButton!
+    @IBOutlet weak var playerView: PlayerView!
+    
     var movie : Movie?
+    let player = AVQueuePlayer()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +31,53 @@ class PlayTrailerController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // ------------------------------------
+    // MARK: Properties
+    // ------------------------------------
+    
+    var currentTime: Double {
+        get {
+            return CMTimeGetSeconds(player.currentTime())
+        }
+        
+        set {
+            let newTime = CMTimeMakeWithSeconds(newValue, 1)
+            player.seek(to: newTime, toleranceBefore: kCMTimeZero, toleranceAfter: kCMTimeZero)
+        }
+    }
+    
+    var duration: Double {
+        guard let currentItem = player.currentItem else { return 0.0 }
+        
+        return CMTimeGetSeconds(currentItem.duration)
+    }
+    
+    var rate: Float {
+        get {
+            return player.rate
+        }
+        
+        set {
+            player.rate = newValue
+        }
+    }
+    
+    var playerLayer: AVPlayerLayer? {
+        return playerView.playerLayer
+    }
+    
+    /*
+     A formatter for individual date components used to provide an appropriate
+     value for the `startTimeLabel` and `durationLabel`.
+     */
+    let timeRemainingFormatter: DateComponentsFormatter = {
+        let formatter = DateComponentsFormatter()
+        formatter.zeroFormattingBehavior = .pad
+        formatter.allowedUnits = [.minute, .second]
+        
+        return formatter
+    }()
+
 
     /*
     // MARK: - Navigation
